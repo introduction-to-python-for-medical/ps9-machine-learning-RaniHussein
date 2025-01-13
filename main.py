87964800c1652b4a91c4d2ea623f4daca3a99a9b
RaniHussein <ranihu46@gmail.com>
2025-01-13 18:34:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 # Download the data from your GitHub repository
 !wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
-     
+#     
 import pandas as pd
 
 df = pd.read_csv('parkinsons.csv')
